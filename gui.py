30a1ba64ffde3d13f8479824bfe0113a1242e2d6
Vijaym15 <108979347+Vijaym15@users.noreply.github.com>
2023-08-02 20:29:25 +0530

    gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
-
 from sklearn import metrics
-
 from tensorflow.keras.models import model_from_json
 from PIL import Image, ImageTk
 import numpy as np
@@ -11,34 +9,27 @@ import cv2
 
 # download haarcascade_frontalface_default from here "https://github.com/opencv/opencv/tree/master/data/haarcascades"
 
-
 def FacialExpressionModel(json_file, weights_file):
     with open(json_file,"r") as file:
         loaded_model_json = file.read()
         model = model_from_json(loaded_model_json)
-
     model.load_weights(weights_file)
     model.compile(optimizer ='adam', loss='categorical_crossentropy', metrics = ['accuracy'])
-
     return model
 
 top =tk.Tk()
 top.geometry('800x600')
 top.title('Emotion Detector')
 top.configure(background='#CDCDCD')
-
 label1 = Label(top, background='#CDCDCD', font=('arial',15,'bold'))
 sign_image = Label(top)
-
 facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 model = FacialExpressionModel("model_a1.json","model_weights1.h5")
-
 
 EMOTIONS_LIST = ["Angry","Disgust","Fear","Happy","Neutral","Sad","Surprise"]
 
 def Detect(file_path):
     global Label_packed
-
     image = cv2.imread(file_path)
     gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     faces = facec.detectMultiScale(gray_image,1.3,5)
@@ -52,12 +43,10 @@ def Detect(file_path):
     except:
         label1.configure(foreground="#011638",text = "Unable to detect")
 
-
 def show_Detect_button(file_path):
     detect_b = Button(top,text="Detect Emotion", command= lambda: Detect(file_path),padx=10,pady=5)
     detect_b.configure(background="#364156",foreground='white',font=('arial',10,'bold'))
     detect_b.place(relx =0.79,rely=0.46)
-
 
 def upload_image():
     try:
